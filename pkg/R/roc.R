c1b53092150b7345496287da7d510f3dc1eb423e
lml (Leo Lahti) <leo.lahti@iki.fi>
2011-07-17 21:16:24 +0000

    ROC
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/intcomp@40 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/roc.R
+++ pkg/R/roc.R
@@ -13,6 +13,21 @@ roc.auc <- function (ordered.results, P) {
   as.numeric(t(rates$fpr[-1]-rates$fpr[-length(rates$fpr)])%*%rates$tpr[-length(rates$fpr)])
 }
 
+roc.auc2 <- function (ordered.results, P) {
+
+  # Calculate ROC/AUC value for ordered list, given the list P of true
+  # positives
+  
+  # ordered results: best to worst
+  # P: known positives
+
+  # Compute area under curve
+  rates <- roc(ordered.results, P)
+  # integration: Compute step intervals and compute weighted sum of true positive rates in each interval.
+  # note that interval length can be 0 if fpr does not change
+  list(auc = as.numeric(t(rates$fpr[-1]-rates$fpr[-length(rates$fpr)])%*%rates$tpr[-length(rates$fpr)]), tpr = rates$tpr, fpr = rates$fpr)
+}
+
 roc <- function (ordered.results, P) {
 
   # Calculate ROC curve
