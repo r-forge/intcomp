810cb6b3f624912514b91cbd23e86512afbd1b24
lml (Leo Lahti) <leo.lahti@iki.fi>
2014-01-06 13:34:32 +0000

    build
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/intcomp@113 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ build.sh
@@ -0,0 +1,9 @@
+#/usr/bin/R CMD BATCH document.R
+/usr/bin/R CMD check pkg
+/usr/bin/R CMD build pkg
+/usr/bin/R CMD INSTALL pkg_0.3.28.tar.gz
+
+#~/bin/R-2.13.2/bin/R CMD check pkg
+#~/bin/R-2.13.2/bin/R CMD build pkg
+#~/bin/R-2.13.2/bin/R CMD INSTALL sorvi_0.1.29.tar.gz
+
